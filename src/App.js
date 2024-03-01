1b3ef8ff6d234ba3f5dabe24fa0067e622eb8cf1
Jess Liu <jessica040532@gmail.com>
2024-03-01 16:07:04 +1300

    Add eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
+import logo from "./static";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
