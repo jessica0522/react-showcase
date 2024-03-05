a79660aeba06ec6e79399ae229cd3a48dfd5a4e5
Jess Liu <jessica040532@gmail.com>
2024-03-05 15:50:08 +1300

    Install firebase

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig = {
+  apiKey: "AIzaSyDOJeKwLWJiIosGyh0d5VdeSacASPw3Hug",
+  authDomain: "my-react-blog-76711.firebaseapp.com",
+  projectId: "my-react-blog-76711",
+  storageBucket: "my-react-blog-76711.appspot.com",
+  messagingSenderId: "782654509861",
+  appId: "1:782654509861:web:4ac12cb1e878be13b680cc",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
