45a9d39ab91ee7c60a387f8496ba789a4df7eb7c
Jess Liu <jessica040532@gmail.com>
2024-03-26 18:47:39 +1300

    Post page with temporary static data

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,12 +1,67 @@
 import React from "react";
+import { Category, Post } from "../static/interfaces";
+import { PostLists } from "../components";
 
-type Props = {};
+const posts: Post[] = [
+  {
+    id: "post1",
+    title: "Boost your conversion rate",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: Category.Animal,
+    author: {
+      email: "Michael Foster",
+      role: "Co-Founder / CTO",
+    },
+  },
+  {
+    id: "post2",
+    title: "Boost your conversion rate",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: Category.Free,
+    author: {
+      email: "Michael Foster",
+      role: "Co-Founder / CTO",
+    },
+  },
+  {
+    id: "post3",
+    title: "Boost your conversion rate",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: Category.Nature,
+    author: {
+      email: "Michael Foster",
+    },
+  },
+];
 
-const Posts = (props: Props) => {
+const Posts = () => {
   return (
-    <div>
-      <h1>Posts</h1>
-    </div>
+    <>
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Blogs
+            </h2>
+            <p className="mt-2 text-lg leading-8 text-gray-600">
+              You can add your own blog after signing in.
+            </p>
+          </div>
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <PostLists posts={posts} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
